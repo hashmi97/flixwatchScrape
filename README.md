aa960fa19600354a9b7a4bef0c1d3ba556aa9f3f
hashmi97 <h.m.hisham.alhashmi@gmail.com>
2021-05-15 23:47:16 -0700

    Updated logging to include the timestamp

--- README.md
+++ README.md
@@ -1,4 +1,16 @@
-# flixwatchScrape
+# flixwatchScrape tool
 
-Use this tool to scrape https://www.flixwatch.co/ To use it you can just run 
-the following `python main.py`. This will create two files, a log
+
+### Description 
+Use this tool to scrape https://www.flixwatch.co/ website
+
+
+### Usage
+To use the tool, you can just run the following command
+
+`main.py [-h] [-v] [-l LogFile.txt] [-o Output.csv]`
+
+- If the verbose option is enabled, the log file outputs will also be printed to 
+the screen 
+
+- This will create two files, `LogFile.txt` which will hold log detailing what contries were covered at what time and what shows were printed
--- main.py
+++ main.py
@@ -6,25 +6,26 @@ import pandas as pd
 import sys
 import numpy as np
 import argparse
+import time
 
 
 # This method pulls the list of available countries from the website and
 # there is a bug at the moment where Mongolia is missing on the website which
 # is why I have it hardcoded
 def get_list_of_countries():
-    country_url = "https://www.flixwatch.co/catalogs/"
+    country_url = 'https://www.flixwatch.co/catalogs/'
     country_res = requests.get(country_url)
     country_html_page = country_res.content
     country_soup = BeautifulSoup(country_html_page, 'html.parser')
     countries = np.array([])
     for web_line in country_soup.find_all(text=True):
-        if web_line.parent.name in ["a", "details"]:
+        if web_line.parent.name in ['a', 'details']:
             countries = np.append(countries, (web_line.parent.name, web_line))
-    countries = countries[np.min(np.where(countries == "details")):
-                          np.max(np.where(countries == "details"))]
+    countries = countries[np.min(np.where(countries == 'details')):
+                          np.max(np.where(countries == 'details'))]
     countries = countries[np.logical_and(
-        countries != "details", np.logical_and(countries != "a", countries !=
-                                               "\n"))]
+        countries != 'details', np.logical_and(countries != 'a', countries !=
+                                               '\n'))]
     countries = np.append(countries, 'Mongolia')
     np.unique(countries).sort()
     return countries
@@ -34,14 +35,14 @@ def get_list_of_countries():
 # now but in the case it gets changed or increased then the number is updated
 # here.
 def get_max_page_num():
-    page_num_url = "https://www.flixwatch.co/catalogue/netflix-usa"
+    page_num_url = 'https://www.flixwatch.co/catalogue/netflix-usa'
     page_num_res = requests.get(page_num_url)
     page_num_html_page = page_num_res.content
     page_num_soup = BeautifulSoup(page_num_html_page, 'html.parser')
     max_page_n = 0
     for line in page_num_soup.find_all(text=True):
         if line.parent.name == 'a':
-            if "Next" in line:
+            if 'Next' in line:
                 break
             max_page_n = line
     return max_page_n
@@ -55,32 +56,34 @@ def parse_show_page(array, show_type):
     global df, log_file
     array = array[11:-5]
     show_name = array[0]
-    print('Now working on show {}'.format(show_name), file=log_file)
-    print('Now working on show {}'.format(show_name))
+    print(time.strftime('[%d/%m/%Y %H:%M:%S %zGMT]: ', time.gmtime()) +
+          'Now working on show {}'.format(show_name), file=log_file)
+    print(time.strftime('[%d/%m/%Y %H:%M:%S %zGMT]: ', time.gmtime()) +
+          'Now working on show {}'.format(show_name))
     show_description = array[2]
-    show_genre = "".join(
-        array[np.where(array == "Genre:")[0] + 1]).strip()
-    show_alt_genre = "".join(array[np.where(
-        array == "Alternate Genre:")[0] + 1]).strip()
+    show_genre = ''.join(
+        array[np.where(array == 'Genre:')[0] + 1]).strip()
+    show_alt_genre = ''.join(array[np.where(
+        array == 'Alternate Genre:')[0] + 1]).strip()
     show_streaming_countries = ''
-    i = np.where(array == "Streaming in:")[0] + 1
-    while array[i] != "IMDb:":
+    i = np.where(array == 'Streaming in:')[0] + 1
+    while array[i] != 'IMDb:':
         show_streaming_countries = (show_streaming_countries +
                                     str(array[i][0])).strip()
         i += 1
     show_IMDb_score = array[
-        np.where(array == "IMDb:")[0] + 1][0].strip()
+        np.where(array == 'IMDb:')[0] + 1][0].strip()
     show_metacritic_score = array[np.where(
-        array == "Metacritic:")[0] + 1][0].strip()
+        array == 'Metacritic:')[0] + 1][0].strip()
     show_age_restriction = array[
-        np.where(array == "Suitable for Age ")[0] + 3][0]
+        np.where(array == 'Suitable for Age ')[0] + 3][0]
     show_family_friendly = array[
-        np.where(array == "Family Friendly:")[0] + 1][0].strip()
+        np.where(array == 'Family Friendly:')[0] + 1][0].strip()
     show_year = int(
-        array[np.where(array == "Year:")[0] + 1])
-    show_audio = array[np.where(array == "Audio:")
+        array[np.where(array == 'Year:')[0] + 1])
+    show_audio = array[np.where(array == 'Audio:')
                        [0] + 1][0].strip()
-    if show_metacritic_score == "":
+    if show_metacritic_score == '':
         show_metacritic_score = np.NAN
     else:
         show_metacritic_score = int(show_metacritic_score[:-4])
@@ -89,7 +92,7 @@ def parse_show_page(array, show_type):
     else:
         show_IMDb_score = float(show_IMDb_score[:-3])
     if len(show_age_restriction.strip()) < 2 or \
-            show_age_restriction[-1] != "+":
+            show_age_restriction[-1] != '+':
         show_age_restriction = np.NAN
     else:
         show_age_restriction = int(show_age_restriction[:-1])
@@ -110,13 +113,16 @@ def scrape_website():
         for page_num in range(1, max_page_num + 1):
             try:
                 if verbose:
-                    print("Now working on page: {} of country: {}".format(
-                        page_num, country))
-                print("Now working on page: {} of country: {}".format(page_num,
-                                                                      country),
-                      file=log_file)
-                url = "https://www.flixwatch.co/catalogue/netflix-{}/?" \
-                      "paged={}".format(country.lower().replace(" ", "-"),
+                    print(time.strftime('[%d/%m/%Y %H:%M:%S %zGMT]: ',
+                          time.gmtime()) + 'Now working on page {} of '
+                                           'country {}'.format(
+                                                         page_num, country))
+                print(time.strftime('[%d/%m/%Y %H:%M:%S %zGMT]: ',
+                                    time.gmtime()) + 'Now working on page {}'
+                                                     ' of country {}'.format(
+                                    page_num, country), file=log_file)
+                url = 'https://www.flixwatch.co/catalogue/netflix-{}/?' \
+                      'paged={}'.format(country.lower().replace(' ', '-'),
                                         page_num)
                 res = requests.get(url)
                 html_page = res.content
@@ -138,21 +144,23 @@ def scrape_website():
                                                     'h1']:
                                 show_array = np.append(show_array, line)
                         if 'movies' in curr_show:
-                            parse_show_page(show_array, show_type="movie")
+                            parse_show_page(show_array, show_type='movie')
                         else:
-                            parse_show_page(show_array, show_type="tvshow")
+                            parse_show_page(show_array, show_type='tvshow')
 
             except Exception as e:
                 if verbose:
-                    print(
-                        'Failed while creating show link table at country = '
-                        '{} and page_num = {} due to exception {}'.format(
-                            country, page_num, e))
-                print(
-                    'Failed while creating show link table at country = {} and'
-                    ' page_num = {} due to exception {}'.format(country,
-                                                                page_num, e),
-                    file=log_file)
+                    print(time.strftime('[%d/%m/%Y %H:%M:%S %zGMT]: ',
+                          time.gmtime()) + 'Failed while creating show link '
+                                           'table at country = {} and  '
+                                           'page_num = {} due to exception {}'
+                          .format(country, page_num, e))
+                print(time.strftime('[%d/%m/%Y %H:%M:%S %zGMT]: ',
+                      time.gmtime()) + 'Failed while creating show link table '
+                                       'at country = {} and  page_num = {} due'
+                                       ' to exception {}'.format(country,
+                                                                 page_num, e),
+                      file=log_file)
                 log_file.close()
                 traceback.print_exc(file=log_file)
                 traceback.print_exc()
@@ -160,19 +168,19 @@ def scrape_website():
 
 
 if __name__ == '__main__':
-    parser = argparse.ArgumentParser(usage="main.py [-h] [-v] [-l LogFile.txt]"
-                                           " [-o Output.csv]",
-                                     description="Scrape www.flixwatch.co"
-                                                 "website and create a csv "
-                                                 "file with the data.")
-    parser.add_argument("-v", "--verbose", action="store_true", help='Setting '
+    parser = argparse.ArgumentParser(usage='main.py [-h] [-v] [-l LogFile.txt]'
+                                           ' [-o Output.csv]',
+                                     description='Scrape www.flixwatch.co'
+                                                 'website and create a csv '
+                                                 'file with the data.')
+    parser.add_argument('-v', '--verbose', action='store_true', help='Setting '
                         'the Verbose option enables printing the log files to '
                         'the screen in real time')
-    parser.add_argument('-l', '--log', required=False, default="log.txt",
+    parser.add_argument('-l', '--log', required=False, default='log.txt',
                         help='Pass the name of the log file you want to create'
                              '. The name must end in .txt the default is '
                              'log.txt ')
-    parser.add_argument('-o', '--out', required=False, default="dataset.csv",
+    parser.add_argument('-o', '--out', required=False, default='dataset.csv',
                         help='Pass the name of the csv file you want to create'
                              '. The name must end in .csv the default is '
                              'dataset.csv')
@@ -181,29 +189,25 @@ if __name__ == '__main__':
     log_path = io_args.log
     output_path = io_args.out
     try:
-        if not log_path.endswith(".txt"):
+        if not log_path.endswith('.txt'):
             raise IOError
-        log_file = open(log_path, "w", encoding="utf-8")
+        log_file = open(log_path, 'w', encoding='utf-8')
     except IOError:
         if verbose:
-            print("Was not able to create the log file")
+            print('Was not able to create the log file')
             traceback.print_exc()
         sys.exit(0)
     try:
-        if not output_path.endswith(".csv"):
+        if not output_path.endswith('.csv'):
             raise IOError
         elif os.path.isfile(output_path):
-            csv_file = open(output_path, "w")
+            csv_file = open(output_path, 'w')
             csv_file.close()
     except IOError:
         if verbose:
-            print("Was not able to create the csv file")
+            print('Was not able to create the csv file')
             traceback.print_exc()
             sys.exit(0)
-    # print(verbose)
-    # print(log_path)
-    # print(output_path)
-    # sys.exit(25)
     max_page_num = int(get_max_page_num())
     country_list = get_list_of_countries()
     shows_hash_table = {}
@@ -216,7 +220,10 @@ if __name__ == '__main__':
     df = df.sort_index()
     df.to_csv(output_path, index=False)
     if verbose:
-        print("File {} was created.".format(output_path))
-    print("File {} was created.".format(output_path), file=log_file)
+        print(time.strftime('[%d/%m/%Y %H:%M:%S %zGMT]: ', time.gmtime()) +
+              'File {} was created.'.format(output_path))
+    print(time.strftime('[%d/%m/%Y %H:%M:%S %zGMT]: ',
+                        time.gmtime()) + 'File {} was created.'
+          .format(output_path), file=log_file)
     log_file.close()
     sys.exit(1)
